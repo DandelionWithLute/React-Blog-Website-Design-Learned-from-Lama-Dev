21a003b534f7df70cb54bbbe7d9e9911e08b18ee
OrangeBirdDrinksWater <114991717+OrangeBirdDrinksWater@users.noreply.github.com>
2023-01-19 22:29:56 +0800

    Initial commit

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,31 @@
+import './topbar.css'
+
+export default function TopBar() {
+  return (
+    <div className='top'>
+      <div className='topLeft'>
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className='topListItem'>HOME</li>
+          <li className='topListItem'>ABOUT</li>
+          <li className='topListItem'>CONTACT</li>
+          <li className='topListItem'>WRITE</li>
+          <li className='topListItem'>LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          className='topImg'
+          src=''
+          alt=''
+        />
+        <i className='topSearchIcon fas fas-search'></i>
+      </div>
+    </div>
+  )
+}
